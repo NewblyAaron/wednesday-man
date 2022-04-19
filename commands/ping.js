87b282bc128675a6352fa9404441ac6c3ff921d6
Aaron <amserrano1337@gmail.com>
2022-04-20 04:19:26 +0800

    added ping easter egg

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		const number = Math.floor(Math.random() * 101);
+		const number = Math.floor(Math.random() * 100) + 1;
 		console.log(number);
 		if (number == 1) {
 			console.log(`${interaction.user} is a lucky man`);
